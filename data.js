f046847a7b9af4104d2e80aa7c4a753672610181
Vriscokkk <vrisco88andreas@gmail.com>
2025-12-11 17:48:34 +0700

    menambahkan data.js dari vrisco

---
+++ data.js
@@ -0,0 +1,37 @@
+const projectData = [
+  {
+    id: 1,
+    title: "Analisis Kebutuhan Sistem",
+    assignee: "Budi",
+    status: "done", // opsi: to-do, in-progress, done
+    priority: "Tinggi"
+  },
+  {
+    id: 2,
+    title: "Desain UI (Figma)",
+    assignee: "Siti",
+    status: "in-progress",
+    priority: "Sedang"
+  },
+  {
+    id: 3,
+    title: "Setup Database Firebase",
+    assignee: "Andi",
+    status: "to-do",
+    priority: "Tinggi"
+  },
+  {
+    id: 4,
+    title: "Membuat Laporan Bab 1",
+    assignee: "Budi",
+    status: "to-do",
+    priority: "Rendah"
+  },
+  {
+    id: 5,
+    title: "Testing & Debugging",
+    assignee: "Tim",
+    status: "to-do",
+    priority: "Sedang"
+  }
+];
